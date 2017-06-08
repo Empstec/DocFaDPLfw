76cea91352c24b9353c1786dc214ef685816b7b6
Empstec <salva_puertes@hotmail.com>
2017-06-08 17:10:58 +0200

    SpringBoot

---
+++ DocFacDPLfw/src/main/java/com/UPV/MITSS/TFM/DocFacDPLfw/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.UPV.MITSS.TFM.DocFacDPLfw;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DocFacDpLfwApplication.class);
+	}
+
+}
