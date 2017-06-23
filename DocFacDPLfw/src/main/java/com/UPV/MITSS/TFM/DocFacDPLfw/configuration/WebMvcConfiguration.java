97c678d6e069164b333a10941ac6a1b75dc24d25
Empstec <salva_puertes@hotmail.com>
2017-06-23 16:58:48 +0200

    PreZaragoza

---
+++ DocFacDPLfw/src/main/java/com/UPV/MITSS/TFM/DocFacDPLfw/configuration/WebMvcConfiguration.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.UPV.MITSS.TFM.DocFacDPLfw.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author S
+ */
+
+@Configuration
+
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter{
+    
+    /*private component variable
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(vaiable);
+    }*/
+}
--- DocFacDPLfw/src/main/java/com/UPV/MITSS/TFM/DocFacDPLfw/controller/indexController.java
+++ DocFacDPLfw/src/main/java/com/UPV/MITSS/TFM/DocFacDPLfw/controller/indexController.java
@@ -8,16 +8,34 @@ package com.UPV.MITSS.TFM.DocFacDPLfw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
- * @author S
+ * @author Salvador Puertes Aleixandre
  */
+
 @Controller
-@RequestMapping("/home")
+@RequestMapping("")
 public class indexController {
+    
+    public static final String LOGIN_VEIW = "index";
+    public static final String HOME_VEIW = "home";
+    public static final String REG_VEIW = "registration";
+    
     @GetMapping("/index")
     public String index(){
-        return "in.jsp";
+        return LOGIN_VEIW;
+    }
+    
+    @GetMapping("/registration")
+    public String registration(){
+        return REG_VEIW;
+    }
+    
+    @GetMapping("/home")
+    public String home(){
+        return HOME_VEIW;
     }
 }
+ 
