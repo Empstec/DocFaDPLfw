e4422aff3fa3fba241484d53aa1d34f4097be1ba
Empstec <salva_puertes@hotmail.com>
2017-10-18 10:22:49 +0200

    ToogleSwitch
    Cambios de como se ven los radio actualizada. Trabajo previo a la
    edición de documentos.

--- DocFacDPLfw/src/main/resources/static/js/validateProfile.js
+++ DocFacDPLfw/src/main/resources/static/js/validateProfile.js
@@ -1,7 +1,15 @@
 $("form").submit(function( event ) {
+    event.stopImmediatePropagation();
+    if($("#switch-sex").prop("checked"))
+        $("#switch-sex").val("M");
+    else
+        $("#switch-sex").val("F");
+    
+    $("#switch-sex").prop( "checked", true );
+    
     if($("#old").val()!==""){
         if($("#pwd1").val()===$("#pwd2").val()){
-        $("#pwd2").prop('disabled',true);  
+            $("#pwd2").prop('disabled',true);  
             return;
         }
         $("#pwdError").show();
--- DocFacDPLfw/src/main/resources/static/js/validateReg.js
+++ DocFacDPLfw/src/main/resources/static/js/validateReg.js
@@ -1,7 +1,14 @@
-$("#radioM").prop('checked', true);
+$("#switch-sex").prop('checked', true);
 
 $("form").submit(function( event ) {
     
+    if($("#switch-sex").prop("checked"))
+        $("#switch-sex").val("M");
+    else
+        $("#switch-sex").val("F");
+    
+    $("#switch-sex").prop( "checked", true );
+
     if($("#pwd1").val()===$("#pwd2").val() && $("#pwd1").val()!=="" && $("#pwd2").val()!=="" ){
     $("#pwd2").prop('disabled',true);  
         return;
